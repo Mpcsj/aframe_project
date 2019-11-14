f1f216abb08d5ba7d149fc40865100875c70252a
mpcsj <marcosjunioribm@gmail.com>
2019-11-13 21:00:35 -0300

    trabalho de web-gl e alterações no trab final

---
+++ meu_cod.js
@@ -0,0 +1,20 @@
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+function drawBasicScene(){
+    var sceneEl = document.querySelector('a-scene');
+    console.log('sceneEl: ',sceneEl)
+    let atual = 0.7
+    for(let i=0;i<30;i++){
+        console.log('atual: ',atual)
+        let elAtual =`<a-cylinder position="0 ${atual} -13" rotation="90 90 0 " radius="0.02" height="18" depth="0.2" color="#570909"></a-cylinder>`
+        sceneEl.appendChild(htmlToElement(elAtual))
+        atual+=0.2       
+    }
+
+}
+
+drawBasicScene()
