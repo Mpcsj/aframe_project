068d60f8221adab88190389ade5b43eff5402142
mpcsj <marcosjunioribm@gmail.com>
2019-11-20 21:56:08 -0300

    Mudança de arquivo no index.html

--- meu_cod.js
+++ meu_cod.js
@@ -1,19 +1,27 @@
+const sceneEl = document.querySelector('a-scene');
 function htmlToElement(html) {
     var template = document.createElement('template');
     html = html.trim(); // Never return a text node of whitespace as the result
     template.innerHTML = html;
     return template.content.firstChild;
 }
-function drawBasicScene(){
-    var sceneEl = document.querySelector('a-scene');
-    console.log('sceneEl: ',sceneEl)
-    let atual = 0.7
-    for(let i=0;i<30;i++){
-        let elAtual =`<a-cylinder position="0 ${atual} -13" rotation="90 90 0 " radius="0.02" height="18" depth="0.2" color="#570909"></a-cylinder>`
-        sceneEl.appendChild(htmlToElement(elAtual))
-        atual+=0.2       
-    }
 
+function checkKey(e){
+    console.log('keycode: ',e.keyCode)
+    //getPos()
+    console.log('position: ',sceneEl.object3D.position)
+}
+function detectKeyPress(){
+    document.onkeypress = function (e) {
+        console.log('tecla pressionada: ',e.key )
+    };
+    document.onkeydown = checkKey
+}
+function drawBasicScene(sceneEl){
+    console.log('drawBasicScene')
+    console.log('sceneEl: ',sceneEl)
+    detectKeyPress()
+    
 }
 
-drawBasicScene()
+drawBasicScene(sceneEl)
