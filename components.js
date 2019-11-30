3d7733f979c04304e2220e3621dd8a2870f5235a
Lucas <lucashenriques.dev@gmail.com>
2019-11-30 13:23:32 -0300

    Updates

--- components.js
+++ components.js
@@ -1,36 +1,38 @@
 var generatedElements = [];
+var maxElements = 700;
 
 const addNewCircle = () => {
 
-    if (generatedElements.length >= 150) {
+    if (generatedElements.length >= maxElements) {
         return;
     }
 
-    const positionX = generateRandomValue(250);
-    const positionY = generateRandomValue(250);
-    const duraction = Math.max(generateRandomValue(400, true), 1500);
+    const positionX = generateRandomValue(400);
+    const positionY = generateRandomValue(400);
+    const duraction = Math.max(generateRandomValue(3000, true), 3000);
 
     const time = new Date().getTime();
     const id = `sphere-${time}`;
 
     const html = `
-    <a-entity id="${id}" position="${positionX} 50 ${positionY}" animation="property: position; to: ${positionX} -50 ${positionY}; loop: true dur: ${duraction}"
-        static-body physics-collider="ignoreSleep: true" geometry="primitive: sphere; radius: 1.5">
+    <a-entity id="${id}" position="${positionX} 60 ${positionY}" animation="property: position; to: ${positionX} -20 ${positionY}; loop: true; dur: ${duraction}"
+        static-body physics-collider="ignoreSleep: true" geometry="primitive: sphere; radius: 5">
     </a-entity>
     `;
 
-    $('#scene').append(html);
+    $('#city-area').append(html);
     generatedElements.push({ id, expiresIn: (time + duraction) })
 }
 
+// unused
 const removeExpired = () => {
     const time = new Date().getTime();
     generatedElements.forEach(i => {
         if (time >= i.expiresIn) {
             // nao funciona e nao faco ideia porque. ta na documentação
             // try {
-                // document.querySelector('#scene').querySelector(`#${i.id}`).destroy();
-                // $(`#${i.id}`).remove();
+            // document.querySelector('#scene').querySelector(`#${i.id}`).destroy();
+            // $(`#${i.id}`).remove();
             // } catch (e) { }
         }
     })
@@ -42,17 +44,21 @@ const generateRandomValue = (range, positive) => {
     if (positive) {
         return value;
     }
-    return Math.round() > 0.5 ? value : -value;
+    return Math.random() > 0.5 ? value : (-1 * value);
 }
 
 $(document).ready(() => {
 
-    setInterval(() => {
-        for (let i = 0; i < 20; i++) {
+    const limitPerTime = 40;
+    for (let i = 1; i <= maxElements; i++) {
+        if (i % limitPerTime == 0) {
+            setTimeout(() => {
+                addNewCircle();
+            }, 300)
+        } else {
             addNewCircle();
         }
-        removeExpired();
-    }, 1000)
+    }
 
 
 })
